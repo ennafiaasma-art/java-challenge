ea0690431973d75716f3a8ee2c714fa3df72b375
ennafiaasma-art <ennafiaasma@gmail.com>
2025-11-05 12:39:36 +0100

    Refactor Main.java for banking functionality
    Removed old number guessing game code and added a banking menu.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,27 +33,69 @@ public class Main {
 
  */
 
-        Scanner mine = new Scanner(System.in);
-        System.out.println("entrer un nombre ");
-        int n = mine.nextInt();
-        System.out.println(n);
-        int i = 0;
-        while (i<= 10) {
+/*
+        Random challenge = new Random();
+        Scanner nombre = new Scanner(System.in);
+        int n = 0;
+        int num = challenge.nextInt(1,10);
+        do {
+            System.out.println("entrer un nombre ");
+             n = nombre.nextInt();
+            if (n == num) {
+                System.out.println("Bravo ! Vous avez trouvé.\n");
 
-            System.out.println(i);
-            i++;
+            }else if(n < num){
+                System.out.println("Trop petit ! Essayez encore.");
+            }
+            else {
+                System.out.println("Trop grande ! Essayez encore.");
+            }
 
-if (i==5){
-    System.out.println("Bravo ! Vous avez trouvé.");
-    int randomNum = (int) (Math.random() * 11);
-}else if (i!=5){
-    System.out.println("Trop petit ! Essayez encore.\n");
-    int randomNum = (int) (Math.random() * 11);
 
-}
+        }while (n != num);
+      /*
 
+
+
+ */
+
+
+        Scanner nombre = new Scanner(System.in);
+        System.out.println("  \n1.consolter le solde  \n2.depot  :\n3.retait \n4.quitter \n     entrer votre choix :");
+        int n = nombre.nextInt();
+        if (n==1){
+            System.out.println("votre solde est  100000 DH ");
+        }else if (n==2){
+            System.out.println("entrer un montant : ");
+            n=nombre.nextInt();
+            System.out.println("Dépôt effectué avec succès");
+        } else if (n==3) {
+            System.out.println("entrer un retrait :");
+            n=nombre.nextInt();
+            System.out.println("retrait  effectué avec succès");
+
+
+        }else if(n==4){
+            System.out.println("LA FIN DE PROGRAMME :");
         }
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
